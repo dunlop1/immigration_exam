c402a227359d521ea030ab9985089c9636e396c3
Erika Miwa <erikamiwa612@gmail.com>
2019-11-14 11:05:01 -0800

    implement_home

---
+++ immigration_exam/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './reset.css';
+import './App.css';
+import Nav from '../Nav/Nav';
+import Home from '../Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
