5d9239a18ce32d4c68308bf784afcc9c439faaef
Erika Miwa <erikamiwa612@gmail.com>
2019-11-14 17:54:47 -0800

    implement side nav

--- immigration_exam/src/components/App/App.js
+++ immigration_exam/src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './reset.css';
 import './App.css';
 import Nav from '../Nav/Nav';
+import SideNav from '../SideNav/SideNav';
 import Home from '../Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <SideNav />
       <Home />
     </div>
   );
